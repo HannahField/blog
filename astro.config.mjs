d102f98e0ac08e0827186f8738cbb20c14d6c747
Hannah S. <thathannahfield@gmail.com>
2025-10-16 23:06:38 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'; 
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+     remarkPlugins: [remarkMath],
+     rehypePlugins: [rehypeKatex],
+   },
 });
